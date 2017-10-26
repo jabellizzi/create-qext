8a77c4dacc84340babdfdc289cd85d1f009543ca
John Bellizzi <jabellizzi406@gmail.com>
2017-10-26 14:58:50 -0400

    updated source template files

--- src_files/definition.js
+++ src_files/definition.js
@@ -2,8 +2,34 @@ export default {
   type: "items",
   component: "accordion",
   items: {
-    settings: {
-      uses: "settings"
+    dimensions: {
+      uses: "dimensions",
+      min: 1,
+      max: 1
+    },
+    measures: {
+      uses: "measures",
+      min: 1,
+      max: 1
+    },
+    sorting: {
+      uses: "sorting"
+    },
+    addons: {
+      uses: "addons",
+      items: {
+        dataHandling: {
+          uses: "dataHandling"
+        }
+      }
+    },
+    appearance: {
+      uses: "settings",
+      items: {
+        general: {
+          uses: "general"
+        }
+      }
     }
   }
 }
--- src_files/index.js
+++ src_files/index.js
@@ -1,3 +1,4 @@
+import template from './template.html';
 import initialProperties from './initial-properties.json';
 import definition from './definition.js';
 import paint from './paint.js';
@@ -5,6 +6,7 @@ import localCSS from './style.scss';
 
 export default window.define(['qlik'], function(qlik) {
   return {
+    template: template,
     initialProperties: initialProperties,
     definition: definition,
     paint: paint
